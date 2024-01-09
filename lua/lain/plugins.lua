e4a9d89d152940d3377326323babc3e8cf773b18
LainM <145809470+Lain62@users.noreply.github.com>
2024-01-09 15:45:25 +0700

    deleted dracula
    somehow thats still there and i dont know how

--- lua/lain/plugins.lua
+++ lua/lain/plugins.lua
@@ -2,7 +2,6 @@ return {
 	'nvim-lua/plenary.nvim',
 	{'nvim-telescope/telescope.nvim', tag = '0.1.5',dependencies = { 'nvim-lua/plenary.nvim' }},
 	'nvim-tree/nvim-web-devicons',
-	'Mofiqul/dracula.nvim',
 	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 	{"ThePrimeagen/harpoon", branch = "harpoon2" },
 	'stevearc/oil.nvim',
