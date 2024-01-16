366b67cd8e30008a0dcf14b476087391ef371d83
lain62 <145809470+Lain62@users.noreply.github.com>
2024-01-16 10:22:36 +0700

    more clean up

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,4 +1,4 @@
 
 require("gruvbox").setup()
-vim.o.background = "dark" -- or "light" for light mode
+vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
--- after/plugin/eyeliner.lua
+++ after/plugin/eyeliner.lua
@@ -1,6 +1,6 @@
 require('eyeliner').setup {
-  highlight_on_key = true, -- show highlights only after keypress
-  dim = true              -- dim all other characters if set to true (recommended!)
+  highlight_on_key = true,
+  dim = true            
 }
 
 vim.api.nvim_set_hl(0, 'EyelinerPrimary', { fg='#ffffff',bold = true, underline = true })
