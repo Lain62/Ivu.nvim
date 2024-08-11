f02ec79213fc0cbc5e18e4ffaef101916c4b4485
lain62 <laimadelain@gmail.com>
2024-08-11 16:31:20 +0700

    fixed deprecated harpoon and added name

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,5 +1,17 @@
+local function name()
+  return [[Lain]]
+end
+
 require('lualine').setup {
   options = {
     theme = 'moonfly'
-  }
+  },
+    sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename' },
+    lualine_x = {'encoding', name, 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
 }
