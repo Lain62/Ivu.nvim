347fbb378246e69b69ee89b83c5013a10e9e390b
lain62 <laimadelain@gmail.com>
2024-08-13 23:31:57 +0700

    setup lsp zero
    actually ig i like lsps after all x3

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -2,8 +2,8 @@ require("toggleterm").setup({
   direction = 'tab',
 })
 
-vim.keymap.set ("n", "<C-t>", "<cmd>ToggleTerm<cr>") -- delete buffer
-vim.keymap.set ("t", "<C-t>", "<cmd>ToggleTerm<cr>") -- delete buffer
-vim.keymap.set ("t", "<C-h>", "<cmd>-tabnext<cr>") -- delete buffer
-vim.keymap.set ("t", "<C-l>", "<cmd>+tabnext<cr>") -- delete buffer
+vim.keymap.set ("n", "<C-t>", "<cmd>ToggleTerm<cr>")
+vim.keymap.set ("t", "<C-t>", "<cmd>ToggleTerm<cr>")
+vim.keymap.set ("t", "<C-h>", "<cmd>-tabnext<cr>")
+vim.keymap.set ("t", "<C-l>", "<cmd>+tabnext<cr>")
 
--- lua/Ivu/plugins.lua
+++ lua/Ivu/plugins.lua
@@ -9,5 +9,12 @@ return {
   "jinh0/eyeliner.nvim",
   { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
   { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
-  { 'crispgm/nvim-tabline', dependencies = { 'nvim-tree/nvim-web-devicons' } }
+  { 'crispgm/nvim-tabline', dependencies = { 'nvim-tree/nvim-web-devicons' } },
+  -- lsp zero
+  {'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'},
+  {'neovim/nvim-lspconfig'},
+  {'hrsh7th/cmp-nvim-lsp'},
+  {'hrsh7th/nvim-cmp'},
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim"
 }
