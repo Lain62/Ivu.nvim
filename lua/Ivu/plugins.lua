114a06436dbde5e20c1a90fd042cef649edcdd27
lain62 <laimadelain@gmail.com>
2025-08-10 02:33:31 +0700

    tsinstall cannot be lazy

--- lua/Ivu/plugins.lua
+++ lua/Ivu/plugins.lua
@@ -1,5 +1,5 @@
 return {
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",  branch = 'master', lazy = false },
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope.nvim",   tag = "0.1.5",      dependencies = { "nvim-lua/plenary.nvim" } },
     "nvim-tree/nvim-web-devicons",
